60a86cd460ec9906fa907610d39ae0066a9edbe7
juan-colombo95 <42392347+juan-colombo95@users.noreply.github.com>
2020-04-16 20:02:27 -0300

    bug para que llegue primero la data y despues genere el grafico

--- scripts/graficios.js
+++ scripts/graficios.js
@@ -2,29 +2,30 @@ let xlabels = []
 let xCasesConfirmed = []
 let xCasesDeaths = []
 let xCasesRecovered = []
-chartIt()
-async function chartIt(){
-   await getData()
 
-const ctx = document.getElementById('chart').getContext('2d');
+getData()
 
-const myChart = new Chart(ctx, {
-  type: 'line',
+function chartIt() {
+
+  const ctx = document.getElementById('chart').getContext('2d');
+
+  const myChart = new Chart(ctx, {
+    type: 'line',
     data: {
-        labels: xlabels,
+      labels: xlabels,
 
-        datasets: [{
-            label: 'Casos Confirmado',
-            data: xCasesConfirmed,
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255, 99, 132, 1)',
-            borderWidth: 1
-        },{
-            label: 'Muertes ',
-            data: xCasesDeaths,
-            backgroundColor: 'rgba(63, 127, 191, 0.2)',
-            borderColor: 'rgba(63, 127, 191, 1)',
-            borderWidth: 1
+      datasets: [{
+          label: 'Casos Confirmado',
+          data: xCasesConfirmed,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1
+        }, {
+          label: 'Muertes ',
+          data: xCasesDeaths,
+          backgroundColor: 'rgba(63, 127, 191, 0.2)',
+          borderColor: 'rgba(63, 127, 191, 1)',
+          borderWidth: 1
         },
         {
           label: 'Recuperados',
@@ -32,25 +33,26 @@ const myChart = new Chart(ctx, {
           backgroundColor: 'rgba(36, 175, 24, 0.2)',
           borderColor: 'rgba(36, 175, 24, 1)',
           borderWidth: 1
-      }
+        }
       ]
     },
 
-});
+  });
 }
-async function getData(){
+async function getData() {
 
-fetch("https://juan-colombo95.github.io/AppCodvid19/data/timeseriesArg.json")
-  .then(response => response.json())
-  .then(data => {
-    data["Argentina"].forEach(({ date, confirmed, recovered, deaths }) =>{
+  fetch("https://juan-colombo95.github.io/AppCodvid19/data/timeseriesArg.json")
+    .then(response => response.json())
+    .then(data => {
+      data["Argentina"].forEach(({date,confirmed,recovered,deaths}) => {
         let active = confirmed - recovered - deaths
-        xCasesConfirmed.push( confirmed)
+        xCasesConfirmed.push(confirmed)
         xCasesDeaths.push(deaths)
         xCasesRecovered.push(recovered)
         xlabels.push(date)
 
+        chartIt()
+      });
     });
-  });
 
 }
